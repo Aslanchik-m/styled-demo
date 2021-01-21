e2a836cb4817001ba828c4bb94012f5fe25328b6
Aslanchik <aslan@moveo.co.il>
2021-01-21 18:49:27 +0200

    created all styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, {ThemeProvider} from "styled-components";
+import {colors, device, GlobalStyle} from "./styles/global";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <GlobalStyle/>
+    <ThemeProvider theme={theme}>
+    <Wrapper>
+      <Container>
+      <Button isactive>Primary Button</Button>
+      <Button isactive>Secondary Button</Button>
+      <Button isactive={false}>Inactive Button</Button>
+      <CustomButton isactive>Custom Button</CustomButton>
+      </Container>
+    </Wrapper>
+    </ThemeProvider>
+    </>
+   );
+  }
+  
+  export default App;
+
+const theme = {
+  primary: 'coral',
+  secondary: 'blueviolet'
 }
 
-export default App;
+const Wrapper = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+background-color: ${colors.BLACK};
+height: 100vh;
+`
+const Container = styled.div`
+width: 600px;
+height: 300px;
+display:flex;
+align-items:center;
+justify-content:space-evenly;
+background-color: white;
+transition: all 0.5s ease-in-out;
+
+                @media ${device.laptop}{
+                  background-color:blue;
+                }
+`
+const Button = styled.button<{readonly isactive?:boolean}>`
+display:${props => props.isactive ? 'block' : 'none'};
+height: 30px;
+
+`
+const CustomButton = styled(Button)`
+ height: 60px;
+ color: ${props => props.theme.primary};
+background-color: ${props => props.theme.secondary}
+`
